d5f97f96db8d277f6f72f56c1e4e4f906529ad7d
Joseph <stndent123@yahoo.com.tw>
2020-04-04 23:44:16 +0800

    again

--- SegmentedControlChangePicture/ViewController.swift
+++ SegmentedControlChangePicture/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var outletImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func pressSegmentedControl(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0){
+            outletImageView.image = UIImage(named: "0")
+        }else if(sender.selectedSegmentIndex == 1){
+            outletImageView.image = UIImage(named: "1")
+        }else if(sender.selectedSegmentIndex == 2){
+            outletImageView.image = UIImage(named: "2")
+        }
+    
 
 }
 
+}
