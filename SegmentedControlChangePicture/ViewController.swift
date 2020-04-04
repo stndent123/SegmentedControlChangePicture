b5f7dab522d6a172db3b75636c9c28a32f2a2a81
Joseph <stndent123@yahoo.com.tw>
2020-04-04 22:54:52 +0800

    Initial Commit

---
+++ SegmentedControlChangePicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentedControlChangePicture
+//
+//  Created by joseph on 2020/4/4.
+//  Copyright © 2020 joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
